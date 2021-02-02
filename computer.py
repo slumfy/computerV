2e06e14118da0845f328f13963603712ed6512c9
slumfy <valentin.robert.bransat@gmail.com>
2021-02-02 17:51:51 +0100

    next 3rd

--- computer.py
+++ computer.py
@@ -1,91 +1,157 @@
 #!/usr/bin/python3
 import sys
 
-def Add(i, sign, s):
-	X = 0
-	tmp = i.split('*')
-	for x in tmp:
-		if x.find(s) == -1:
-			X += float(x) * sign
-	return(X)
+class computer:
+	A = 0
+	B = 0
+	C = 0
+	Z = 0
+	sign = 1
+	DELTA = 0
+	degre = 0
 
-def Sqrt(x):
-	lo = 0.0
-	hi = float(x)
-	for i in range(1000):
-		mid = (lo + hi)/2
-		if (mid * mid) == float(x):
-			return mid
-		elif (mid * mid) > float(x):
-			hi = mid
+	def __init__(self,arg_list):
+		for i in arg_list:
+			try:
+				float(i)
+			except:
+				if  i.isnumeric() == False and i != 'X^0' and i != 'X^1' and i != 'X^2' and i != 'X^3' and i != 'X' and i != '+'  and i != '-' and i != '*' and i != '=':
+					print (i)
+					sys.exit("not a valid expression")
+		if arg_list.count('=') != 1:
+			sys.exit("not a valid expression")
+		lenght = len(arg_list) - 1
+		i = 0
+		while i < lenght:
+			if arg_list[i+1] != "+" and arg_list[i+1]  != "-" and arg_list[i+1] != "=" and arg_list[i] != "=" :
+				arg_list[i] = arg_list[i] + arg_list[i+1]
+				arg_list.pop(i + 1)
+				lenght -= 1
+			else:
+				i +=1
+		print (arg_list)
+		for i in arg_list:
+			print (i)
+			print (i.split('*'))
+			if i.find("X^3") != -1:
+				self.Z = self.Add(i, self.sign, "X^3")
+			elif i.find("X^2") != -1:
+				self.A += self.Add(i, self.sign, "X^2")
+			elif i.find("X^0") != -1:
+						self.C += self.Add(i, self.sign,"X^0")
+			elif i.find("X") != -1:
+						self.B += self.Add(i, self.sign,"X")
+			elif i == "=":
+				self.sign = -1
+			else:
+				self.C += float(i) * self.sign
+		self.finding_degre()
+		self.solving_equation()
+
+	def solving_equation(self):
+		self.print_reduction()
+		if self.degre == 3:
+			self.solve_3_degre()
+		elif self.degre == 2:
+			self.solve_2_degre() 
+		elif self.degre == 1:
+			self.solve_1_degre()
+		elif self.degre == 0:
+			self.solve_0_degre()
+
+	def print_reduction(self):
+		print  ("Z = " + str(self.Z))
+		print  ("A = " + str(self.A))
+		print  ("B = " + str(self.B))
+		print  ("C = " + str(self.C))
+		print("degre: ",self.degre)
+		print ("Simplified form: ", end="")
+		if self.Z != 0:
+			print (str(self.Z) + " * X^3 ",end="")
+		if self.A != 0:
+			print (str(self.A) + " * X^2 ",end="")
+		if self.B != 0:
+			print (str(self.B) + " * X",end="")
+		print (str(self.C), "= 0")
+
+	def finding_degre(self):
+		if self.Z != 0:
+			self.degre = 3
+		elif self.A != 0:
+			self.degre = 2
+		elif self.B != 0:
+			self.degre = 1
 		else:
-			lo = mid
-	return mid
+			self.degre = 0
 
-A = 0
-B = 0
-C = 0
-sign = 1
-if   len(sys.argv) < 2 :
-	sys.exit("Need an argument")
-arg = str(sys.argv[1])
-arg_list = arg.split()
-print (arg_list)
-for i in arg_list:
-    try:
-        float(i)
-    except:
-        if  i.isnumeric() == False and i != 'X^0' and i != 'X^1' and i != 'X^2' and i != 'X' and i != '+'  and i != '-' and i != '*' and i != '=':
-        	print (i)
-        	sys.exit("not a valid expression")
-if arg_list.count('=') != 1:
-	sys.exit("not a valid expression")
-lenght = len(arg_list) - 1
-i = 0
-while i < lenght:
-	if arg_list[i+1] != "+" and arg_list[i+1]  != "-" and arg_list[i+1] != "=" and arg_list[i] != "=" :
-		arg_list[i] = arg_list[i] + arg_list[i+1]
-		arg_list.pop(i + 1)
-		lenght -= 1
-	else:
-		i +=1
-print (arg_list)
-for i in arg_list:
-	print (i)
-	print (i.split('*'))
-	if i.find("X^2") != -1:
-		A = Add(i, sign, "X^2")
-	elif i.find("X^0") != -1:
-				C = Add(i, sign,"X^0")
-	elif i.find("X") != -1:
-				B = Add(i, sign,"X")
-	elif i == "=":
-		sign = -1
-	else:
-		C += float(i) * sign
+	def solve_3_degre(self):
+		pass
+	
+	def solve_2_degre(self):
+		self.DELTA = self.B**2 - 4*self.A*self.C
+		print ("DELTA =" + str(self.DELTA))
+		if self.DELTA == 0:
+			X = -self.B / 2 * self.A
+			print ("This polynomial equation have 1 solution X = " + str(X))
+		elif self.DELTA > 0:
+			print ("This polynomial equation have 1 solution X1 = " + str(-self.B) + "+ square(" + str(self.DELTA) + ") / " + str(2* self.A) + "  X2= " + str(-self.B) + "- square(" + str(self.DELTA) + ") / " + str(2* self.A))
+			X1 = (-self.B + self.Sqrt(self.DELTA)) / 2 * self.A
+			X2 = (-self.B - self.Sqrt(self.DELTA)) / 2 * self.A
+			print ("X1 = " + str(X1) + " X2 = " + str(X2))
+		elif self.DELTA < 0:
+			print ("This polynomial equation have 1 solution X1 = " + str(-self.B) + " - i * square(" + str(-self.DELTA) + ") /" + str(2* self.A) + "  X2= " + str(-self.B) + "+ i * square(" + str(-self.DELTA) + ") /" + str(2* self.A))
 
-print  ("A = " + str(A))
-print  ("B = " + str(B))
-print  ("C = " + str(C))
-if A:
-	print ("Simplified form: " + str(A) + "X^2 +" + str(B) + "X  + " + str(C) + " = 0")
-	DELTA = B**2 - 4*A*C
-	print ("DELTA =" + str(DELTA))
-	if DELTA == 0:
-		X = -B / 2 * A
-		print ("This polynomial equation have 1 solution X = " + str(X))
-	elif DELTA > 0:
-		print ("This polynomial equation have 1 solution X1 = " + str(-B) + "+ square(" + str(DELTA) + ") / " + str(2* A) + "  X2= " + str(-B) + "- square(" + str(DELTA) + ") / " + str(2* A))
-		X1 = (-B + Sqrt(DELTA)) / 2 * A
-		X2 = (-B - Sqrt(DELTA)) / 2 * A
-		print ("X1 = " + str(X1) + " X2 = " + str(X2))
-	elif DELTA < 0:
-		print ("This polynomial equation have 1 solution X1 = " + str(-B) + " - i * square(" + str(-DELTA) + ") /" + str(2* A) + "  X2= " + str(-B) + "+ i * square(" + str(-DELTA) + ") /" + str(2* A)) 
-else:
-	if B != 0:
-		X = B / -C
-		print ("This equation have one solution X= " + str(B) + "/" + str(C) + "  or X= " + str(X))
-	elif B == 0 and C != 0:
-		sys.exit("This equation is wrong")
-	elif C == 0:
-		print ("Every Number are solution")
+	def solve_1_degre(self):
+		if self.B != 0:
+				X = -self.C / self.B  
+				print ("This equation have one solution X= " + str(-self.C) + "/" + str(self.B) + "  or X= " + str(X))
+		elif self.B == 0.0 and self.C != 0.0:
+				sys.exit("This equation is wrong")
+
+	def solve_0_degre(self):
+		if self.C == 0.0:
+				print ("Every Number are solution")
+		else:
+				print ("There is no solution and the result is absurd")	
+
+	def Add(self, i, sign, s):
+		V = 0
+		tmp = i.split('*')
+		for x in tmp:
+			if x.find(s) == -1:
+				V += float(x) * sign
+			else:
+				V += 1.0 * sign
+		return(V)
+
+	def power(self,x, y):
+		if (y == 0): return 1
+		elif (int(y % 2) == 0):
+			return (self.power(x, int(y / 2)) *
+				self.power(x, int(y / 2)))
+		else:
+			return (x * self.power(x, int(y / 2)) *
+					self.power(x, int(y / 2)))
+
+	def Sqrt(self, x):
+		lo = 0.0
+		hi = float(x)
+		for i in range(1000):
+			mid = (lo + hi)/2
+			if (mid * mid) == float(x):
+				return mid
+			elif (mid * mid) > float(x):
+				hi = mid
+			else:
+				lo = mid
+		return mid
+
+
+if __name__ == "__main__":
+	if   len(sys.argv) < 2 :
+		sys.exit("Need an argument")
+	arg = str(sys.argv[1])
+	arg_list = arg.split()
+	print (arg_list)
+	computer(arg_list)
+
